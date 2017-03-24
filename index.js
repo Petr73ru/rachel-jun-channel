4b81eb9df5f7cedf5ac757d1d1360475b21d76fe
Peter <I.p.a1@yandex.ru>
2017-03-24 19:42:36 +0400

    Return all videos(I hope so!)

--- index.js
+++ index.js
@@ -1,49 +1,33 @@
 'use strict'
 let
-request = require('request'),
+request = require('./request-module'),
 express = require('express'),
-
-answer = '',
-request_link = {
-	url: 'https://www.googleapis.com/youtube/v3/search',
-    qs: {part: 'snippet', order: 'date', channelId: 'UC4yqcgz49APdbgj0OMv7jpA', maxResults: 50, key: 'AIzaSyAEc6reXtKwVemgtV9MypLcOHE2dnovLMY'},
-    method: 'GET',
-	},
 
 app = express(),
 port = process.env.PORT || 3000
 
 app.get('/', (req, res)=>{
 
-	request(request_link, function (error, response, body) {
-    //Check for error
-    if(error){
-        return console.log('Error:', error);
-    }
+    request((err, data)=> {
+        if (err) {
+            console.error(err)
+        }
 
-    //Check for right status code
-    if(response.statusCode !== 200){
-        return console.log('Invalid Status Code Returned:', response.statusCode);
-    }
-
-    // if all is good
-    console.log('request done!'); 
-
-    body = JSON.parse(body)
-
-    
-    let items = body.items || null,
+        let items = data,
         i=1 //счётчик
-    for (let item of items){
-        res.write(i + ':\n')
-        res.write('title : '+item.snippet.title + '\n'+
-        'description : '+item.snippet.description + '\n'+
-        'videoId : ' +item.id.videoId + '\n\n' )
-        i++
-    }
+        for (let item of items){
+            res.write(i + ':\n')
+            res.write('title : '+item.snippet.title + '\n'+
+            'publishedAt : ' + item.snippet.publishedAt + '\n' +
+            'description : '+item.snippet.description + '\n'+
+            'videoId : ' +item.id.videoId + '\n\n' )
+            i++
+        }
 
-    res.end()
-});
+        res.end()
+    })
+
+
 })
 
 app.listen(port,()=>{
---
+++ request-module.js
@@ -0,0 +1,62 @@
+'use strict'
+var 
+request = require('request'),
+totalVideo, // Sum of all videos
+lastVideoDate,
+
+answer = [] //Return all video items
+
+function getVideoItems(callback){
+
+	request(createRequest_link(null), function (error, response, body) {
+    	//Check for error
+    	if(error){
+        	return console.log('Error: ', error);
+    	}
+
+    	//Check for right status code
+    	if(response.statusCode !== 200){
+        	return console.log('Invalid Status Code Returned: ', response.statusCode);
+    	}
+
+    	// if all is good
+    	console.log('request done!'); 
+
+    	body = JSON.parse(body)
+
+    	if (lastVideoDate === undefined) {
+    		totalVideo = body.pageInfo.totalResults  // Sum of all videos from channel; set only in first request
+    	}
+
+    	let items = body.items || null // Massiv of objects or null
+    	
+    	console.log("totalVideo = " + totalVideo) // Console log
+    	console.log('Total results : ' + body.pageInfo.totalResults)
+    	items.forEach((item, index)=> { // Items push to answer
+    		answer.push(item)
+    	})
+
+    	if (answer.length < totalVideo) {
+    		lastVideoDate = answer[answer.length - 1].snippet.publishedAt // Seting date of last in list video
+    		getVideoItems(callback)
+    	} else {
+    		callback(null, answer)
+    	}
+    
+	});
+}
+
+function createRequest_link(time) {
+	let request_link = {
+		url: 'https://www.googleapis.com/youtube/v3/search',
+    	qs: {part: 'snippet', order: 'date', publishedBefore: lastVideoDate, channelId: 'UC4yqcgz49APdbgj0OMv7jpA', maxResults: 50, key: 'AIzaSyAEc6reXtKwVemgtV9MypLcOHE2dnovLMY'},
+    	method: 'GET',
+		}
+	request_link.publishedBefore = lastVideoDate
+	console.log('publishedBefore : ' + request_link.publishedBefore)
+	return request_link
+}
+
+
+
+module.exports = getVideoItems
